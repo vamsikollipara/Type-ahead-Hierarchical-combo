860cefa5c341174d986b8e0750e329479a32f6b8
Mr_Green <kamlekar.venkatesh@gmail.com>
2015-02-27 16:07:12 +0530

    modified code

--- assets/divcombotree.jquery.js
+++ assets/divcombotree.jquery.js
@@ -3,24 +3,21 @@
   * @author Venkateshwar
   */
 var DivComboTree = function(elem, suggestions) {
-	var code = {up: 38, down: 40},
-		suggestionsContainerClass = ".suggestionsContainer";
+	var code = {up: 38, down: 40,enter: 13},
+		suggestionsContainerClass = ".suggestionsContainer",
+		$elem = $(elem),
+		$suggestionsContainer = $elem.find(suggestionsContainerClass);
+	$elem.addClass('comboTree');
 
-	$(elem).addClass('comboTree');
+	var textBox = document.createElement('input');
+	textBox.setAttribute('type', 'text');
+	renderSuggestions(elem, suggestions, textBox);
+
 	function toggleChildItems(e){
 		var el = $(e.currentTarget);
 
 		if(el.hasClass('collapseArrow')){
-			var refId = el.parent().data('id');
-			for(var i = 0; i< suggestions.length; i++){
-				var p = suggestions[i];
-				if(refId === p.id){
-					p.excludeChildren = true;
-				}
-			}
-			renderSuggestions(elem, suggestions);
-			el.removeClass('collapseArrow')
-				.addClass('expandArrow');
+			collapseItems(el);
 		}
 		else if(el.hasClass('expandArrow')){
 			var refId = el.parent().data('id');
@@ -35,93 +32,95 @@ var DivComboTree = function(elem, suggestions) {
 			if(blnAccess){
 				renderSuggestions(elem, suggestions);
 			}
-			el.removeClass('expandArrow')
-				.addClass('collapseArrow');
 		}
 	}
-	function insertProjectAndFeatureName(e){
-		var el = $(e.currentTarget);
-		var result = el.parents('ul').prev('div').children('span').eq(1).html() + ":" + el.html();
-		el.parents(suggestionsContainerClass).prev('input').val(result);
-		el.parents(suggestionsContainerClass).hide();
+	function collapseItems(el){
+		var refId = el.parent().data('id');
+		for(var i = 0; i< suggestions.length; i++){
+			var p = suggestions[i];
+			if(refId === p.id){
+				p.excludeChildren = true;
+			}
+		}
+		renderSuggestions(elem, suggestions);
 	}
-	function insertProjectName(e){
-		var el = $(e.currentTarget);
-		var input = el.parents(suggestionsContainerClass).prev('input');
-		input.val(el.html());
-		el.parents(suggestionsContainerClass).hide();
+	function insertData(e){
+		var el = $(e.currentTarget || e);
+		var text = $elem.find('input');
+		if(el[0].tagName.toLowerCase() === "span"){
+			text.val(el.html());
+		}
+		else if(el[0].tagName.toLowerCase() === "div"){
+			text.val(el.children.eq(1).html());
+		}
+		else{
+			text.val(el.parents('li').children('div').children('span').eq(1).html() + ":" + el.html());
+		}
+		$elem.find(suggestionsContainerClass).hide();
 	}
 	function showSuggestions(e){
-		$(e.currentTarget).next(suggestionsContainerClass).toggle();
+		filterSuggestions($(e.currentTarget));
+		$elem.find(suggestionsContainerClass).show();
+	}
+	function hideSuggestions(e){
+		$elem.find(suggestionsContainerClass).hide();
 	}
 	function keyOperations(e){
 		var keyCode = e.keyCode || e.which;
 		// console.log(keyCode);
 		var el = $(e.currentTarget);
 		if(code.up == keyCode){
-			var active = el.next(suggestionsContainerClass).find('.active');
+			var active = $elem.find('.active');
+
 			if(active.length){
-				var previous = active.prev();
-				if(previous.length){
-					previous.addClass('active');
-				}
-				else{
-					var parentPrev = active.parent().prev();
-					if(parentPrev.length){
-						var children = parentPrev.find('li');
-						if(children.length){
-							children.last().addClass('active');
+
+				var selectables = $elem.find('.selectable');
+
+				for(var i = 0; i < selectables.length; i++){
+					var s = selectables.eq(i);
+					if(s[0] === active[0]){
+						var k = i - 1;
+						if(k >= 0 && k < selectables.length){
+							selectables.eq(k).addClass('active');
 						}
 						else{
-							parentPrev.addClass('active');
+							selectables.eq(selectables.length-1).addClass('active');
 						}
-					}
-					else{
-						//Select the last feature or project name 
-						el.next(suggestionsContainerClass).children('ul').find('li:last').addClass('active');
-					}
-				}
-				active.removeClass('active');
-			}
-			else{
-				//Select the last feature or project name 
-				el.next(suggestionsContainerClass).children('ul').find('li:last').addClass('active');
-				active.removeClass('active');
-			}
-		}
-		else if(code.down === keyCode){
-			var active = el.next(suggestionsContainerClass).find('.active');
-			if(active.length){
-				var next = active.next();
-				if(next.length){
-					var nextChildren = next.children('li');
-					if(nextChildren.length){
-						nextChildren.first().addClass('active');
-					}
-					else{
-						next.addClass('active');
-					}
-				}
-				else{
-					var nextItem = active.parent().parent().next().children().eq(0);
-					if(nextItem.length){
-						nextItem.addClass('active');
-					}
-					else{
-						//Select the last feature or project name 
-						var item = el.next(suggestionsContainerClass).find('ul > li > div').first();
-						item.addClass('active');
 						active.removeClass('active');
 					}
 				}
-				active.removeClass('active');
 			}
 			else{
-				//Select the last feature or project name 
-				var item = el.next(suggestionsContainerClass).find('ul > li > div').first();
-				item.addClass('active');
-				active.removeClass('active');
+				$elem.find('.selectable').last().addClass('active');
 			}
+		}
+		else if(code.down === keyCode){
+			var active = $elem.find('.active');
+
+			if(active.length){
+
+				var selectables = $elem.find('.selectable');
+
+				for(var i = 0; i < selectables.length; i++){
+					var s = selectables.eq(i);
+					if(s[0] === active[0]){
+						var k = i + 1;
+						if(k >= selectables.length){
+							selectables.eq(0).addClass('active');
+						}
+						else{
+							selectables.eq(k).addClass('active');
+						}
+						active.removeClass('active');
+					}
+				}
+			}
+			else{
+				$elem.find('.selectable').first().addClass('active');
+			}
+		}
+		else if(code.enter === keyCode){
+			insertData(e);
 		}
 		else{
 			filterSuggestions(el);
@@ -130,11 +129,11 @@ var DivComboTree = function(elem, suggestions) {
 
 	function makeActive(e){
 		var el = $(e.currentTarget);
-		el.parents(suggestionsContainerClass).find('.active').removeClass('active');
+		$elem.find('.active').removeClass('active');
 		el.addClass('active');
 	}
 	function filterSuggestions(el){
-		el.next(suggestionsContainerClass).show();
+		$elem.find(suggestionsContainerClass).show();
 		var query = el.val();
 		var queryString = "";
 
@@ -150,6 +149,7 @@ var DivComboTree = function(elem, suggestions) {
 			var p = suggestions[i];
 			if(p.name === queryString){
 				p.excludeParent = false;
+				p.excludeChildren = false;
 				var f = p.children;
 				for(var j=0;j<f.length;j++){
 					var feature = f[j];
@@ -166,20 +166,21 @@ var DivComboTree = function(elem, suggestions) {
 			}
 			else{
 				p.excludeParent = false;
+				p.excludeChildren = true;
 				var f = p.children;
-				for(var j=0;j<f.length;j++){
-					var feature = f[j];
-					feature.excludeChild = false;
-				}				
+				if(f){
+					for(var j=0;j<f.length;j++){
+						var feature = f[j];
+						feature.excludeChild = false;
+					}	
+				}			
 			}
 		}
 		renderSuggestions(elem, suggestions);
 	}
 	function renderSuggestions(el, list, textBox){
 		if(list.length){
-
 			var ul = document.createElement('ul');
-
 
 			for(var i=0; i< list.length; i++){
 				var projectHeadingWrapper = document.createElement('div');
@@ -189,6 +190,8 @@ var DivComboTree = function(elem, suggestions) {
 				projectHeadingWrapper.appendChild(spanArrow);
 				projectHeadingWrapper.appendChild(spanProjectName);
 
+				projectHeadingWrapper.className = "selectable";
+
 				var li = document.createElement('li');
 				li.appendChild(projectHeadingWrapper);
 
@@ -197,20 +200,24 @@ var DivComboTree = function(elem, suggestions) {
 
 				var project = list[i];
 				var features = project.children;
-
-				if(project.excludeChildren === false){
-					spanArrow.className = "collapseArrow";
-				}
-				else{
-					spanArrow.className = "expandArrow";
-				}
-				if(!project.excludeParent){
+				if(project.children){
+					if(project.excludeChildren === false){
+						spanArrow.className = "collapseArrow";
+					}
+					else{
+						spanArrow.className = "expandArrow";
+					}
 					//event
 					spanArrow.onclick = toggleChildItems;					
+				}
+				else{
+					spanArrow.className = "noChild";
+				}
+				if(!project.excludeParent){
 					spanProjectName.innerHTML = project.name;
 					projectHeadingWrapper.setAttribute('data-id', project.id);
 
-					spanProjectName.onclick = insertProjectName;
+					spanProjectName.onclick = insertData;
 
 					li.name = project.id;
 					if(features && features.length && project.excludeChildren === false){
@@ -224,8 +231,10 @@ var DivComboTree = function(elem, suggestions) {
 								subUl.appendChild(subLi);
 
 								//event
-								subLi.onclick = insertProjectAndFeatureName;
+								subLi.onclick = insertData;
 								subLi.onmouseover = makeActive;
+
+								subLi.className = "selectable";
 							}
 						}
 						li.appendChild(subUl);
@@ -239,6 +248,7 @@ var DivComboTree = function(elem, suggestions) {
 
 				//event
 				textBox.onclick = showSuggestions;
+				// textBox.onblur = hideSuggestions;
 				textBox.onkeyup = keyOperations;
 
 				var suggestionsContainer = document.createElement('div');
@@ -253,7 +263,4 @@ var DivComboTree = function(elem, suggestions) {
 			el.appendChild(suggestionsContainer);
 		}	
 	}
-	var textBox = document.createElement('input');
-	textBox.setAttribute('type', 'text');
-	renderSuggestions(elem, suggestions, textBox);
 }
